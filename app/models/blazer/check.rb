d4ba6b53ce5b5723b1a894a44b54f66d8b307ac7
Michael Righi <michael@righi.me>
2016-01-15 00:13:56 -0800

    Add option to not send emails when checks pass or error

--- app/models/blazer/check.rb
+++ app/models/blazer/check.rb
@@ -19,11 +19,34 @@ module Blazer
           invert ? "failing" : "passing"
         end
 
-      # do not notify on creation, except when not passing
-      if (state_was || state != "passing") && state != state_was && emails.present?
+      if notify?
         Blazer::CheckMailer.state_change(self, state, state_was, rows.size, error).deliver_later
       end
       save! if changed?
     end
+
+    protected
+
+      def notify?
+        send_it = true
+        send_it &&= emails.present?
+
+        # Do not notify if the state has not changed
+        send_it &&= (state != state_was)
+
+        # Do not notify on creation, except when not passing
+        send_it &&= (state_was || state != "passing")
+
+        if self.respond_to?(:notify_on_error)
+          # Do not notify on error when notify_on_error is false
+          send_it &&= (state != "error" || notify_on_error)
+
+          # Do not send on passing when notify_on_pass is false, or when notify_on_pass is true but
+          # the previous state was 'error' and notify_on_error is false.
+          send_it &&= (state != "passing" || (notify_on_pass && (state_was != "error" || notify_on_error)))
+        end
+
+        send_it
+      end
   end
 end
